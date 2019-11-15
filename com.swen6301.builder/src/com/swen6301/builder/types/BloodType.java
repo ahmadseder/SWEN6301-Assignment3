84e135082459a31f2c52f0ed15a0676af1e861b7
A. Seder <ahmad@525k.io>
2019-11-15 20:12:20 +0200

    Create BloodType enum to enhance code structure and minimize the validation

---
+++ com.swen6301.builder/src/com/swen6301/builder/types/BloodType.java
@@ -0,0 +1,21 @@
+package com.swen6301.builder.types;
+
+
+public enum BloodType
+{
+    A_POSITIVE("A+"),
+    A_NEGATIVE("A-"),
+    B_POSITIVE("B+"),
+    B_NEGATIVE("B-"),
+    AB_POSITIVE("AB+"),
+    AB_NEGATIVE("AB-"),
+    O_POSITIVE("O+"),
+    O_NEGATIVE("O-");
+
+    private final String label;
+
+    private BloodType(String label) { this.label = label; }
+
+    @Override
+    public String toString() { return label; }
+}
