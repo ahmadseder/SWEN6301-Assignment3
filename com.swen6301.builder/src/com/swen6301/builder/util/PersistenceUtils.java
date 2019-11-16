204bb1c76027d93da75a7964f8f8706be80f3de6
A. Seder <ahmad@525k.io>
2019-11-16 02:15:47 +0200

    Refactor code to use patient object instead of multiple params

--- com.swen6301.builder/src/com/swen6301/builder/util/PersistenceUtils.java
+++ com.swen6301.builder/src/com/swen6301/builder/util/PersistenceUtils.java
@@ -1,5 +1,6 @@
 package com.swen6301.builder.util;
 import com.swen6301.builder.types.BloodType;
+import types.Patient;
 
 /**
  * Contains utilities to communicate with internal storage.
@@ -14,22 +15,15 @@ public final class PersistenceUtils {
 	}
 	
 	/**
-	 * Stores patient's info in internal storage See comments associated with parameter to see required info and non-required info.
-	 * 
-	 * @param firstName Corresponds to the first name of the patient. This cannot be empty.
-	 * @param middleName Corresponds to the middle name of the patient. This can be empty.
-	 * @param lastName Corresponds to the last name of the patient. This cannot be empty.
-	 * @param age Corresponds to the age of the patient. It should be between 0 and 120.
-	 * @param weight Corresponds to the weight of the patient. It should be between 0 and 300kg.
-	 * @param sex Corresponds to the sex of the patient. It should be either 'none', 'male', or 'female', it cannot be empty.
-	 * @param height Corresponds to the height of the patient. It should be between 0 and 250cm.
-	 * @param organDonor Corresponds to whether the patient is an organ donor.
-	 * @param bloodType Corresponds to the blood type of the patient. It must be 'none', 'a+', 'a-', 'o+', 'o-', 'b+', 'b-', 'ab+', 'ab-'.
+	 * Stores patient's info in internal storage
+	 * Check {@link com.swen6301.builder.util.ValidationUtils} to see required info and non-required fields.
+	 *
+	 * @param patient Corresponds to the patient {@link Patient} object.
 	 * @return true, if the patient info confirms to the requirement set forth, otherwise an {@link IllegalArgumentException} will be thrown.
 	 */
-	public static boolean storePatientInfo(String firstName, String middleName, String lastName, int age, int weight, String sex, int height, boolean organDonor, BloodType bloodType) {
+	public static boolean storePatientInfo(Patient patient) {
 		try {
-			ValidationUtils.checkPatientInfo(firstName, middleName, lastName, age, weight, sex, height, organDonor, bloodType);
+			ValidationUtils.checkPatientInfo(patient);
 		} catch (IllegalArgumentException e) {
 			System.out.println("Error: " + e.getMessage());
 			return false;
