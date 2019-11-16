b708ac56b6fe16f6891a3dee9c0e36f8fe8a7561
A. Seder <ahmad@525k.io>
2019-11-16 02:16:45 +0200

    Use patient object, extract to methods to isolate th validation logic per field, and use constants to enhance the readbility

--- com.swen6301.builder/src/com/swen6301/builder/util/ValidationUtils.java
+++ com.swen6301.builder/src/com/swen6301/builder/util/ValidationUtils.java
@@ -1,11 +1,19 @@
 package com.swen6301.builder.util;
 import com.swen6301.builder.types.BloodType;
+import types.Patient;
 
 /**
  * A utility class containing a set of utilities to perform input validations.
  */
 public final class ValidationUtils {
-	
+	private static int MIN_AGE = 0;
+	private static int MAX_AGE = 0;
+	private static int MIN_HEIGHT = 0;
+	private static int MAX_HEIGHT = 0;
+	private static int MIN_WEIGHT = 0;
+	private static int MAX_WEIGHT = 0;
+
+
 	/**
 	 * A private constructor to prevent accidental instantiations of this class.
 	 */
@@ -14,68 +22,84 @@ public final class ValidationUtils {
 	}
 	
 	/**
-	 * Checks whether patient's info is correct. See comments associated with parameter to see required info and non-required info.
+	 * Checks whether patient's info is correct.
 	 * 
-	 * @param firstName Corresponds to the first name of the patient. This cannot be empty.
-	 * @param middleName Corresponds to the middle name of the patient. This can be empty.
-	 * @param lastName Corresponds to the last name of the patient. This cannot be empty.
-	 * @param age Corresponds to the age of the patient. It should be between 0 and 120.
-	 * @param weight Corresponds to the weight of the patient. It should be between 0 and 300kg.
-	 * @param sex Corresponds to the sex of the patient. It should be either 'none', 'male', or 'female', it cannot be empty.
-	 * @param height Corresponds to the height of the patient. It should be between 0 and 250cm.
-	 * @param organDonor Corresponds to whether the patient is an organ donor.
-	 * @param bloodType Corresponds to the blood type of the patient. It must be 'none', 'a+', 'a-', 'o+', 'o-', 'b+', 'b-', 'ab+', 'ab-'.
+	 * @param patient Corresponds to the patient. This cannot be empty.
 	 * @return true, if the patient info confirms to the requirement set forth, otherwise an {@link IllegalArgumentException} will be thrown.
 	 */
-	public static boolean checkPatientInfo(String firstName, String middleName, String lastName, int age, int weight, String sex, int height, boolean organDonor, BloodType bloodType) {
-		
-		// Check whether the firstName is not null and not empty.
-		if(firstName == null || firstName.isEmpty()) {
-			throw new IllegalArgumentException("First name cannot be empty of null.");
-		}
-		
-		// Check whether the middleName is no null.
-		if(middleName == null) {
-			throw new IllegalArgumentException("Middle name cannot be null.");
-		}
-		
-		// Check whether the lastName is not null and not empty.
-		if(lastName == null || lastName.isEmpty()) {
-			throw new IllegalArgumentException("Last name cannot be empty of null.");
-		}
-		
-		// Check whether the age is between 0 and 120.
-		if(age < 0 || age > 120) {
-			throw new IllegalArgumentException("Age cannot be negative or larger than 120.");
-		}
-		
-		// Check whether the weight is between 0 and 300.
-		if(weight < 0 || weight > 300) {
-			throw new IllegalArgumentException("Weight cannot negative or larger than 300.");
-		}
-		
-		// Check whether the height is between 0 and 250.
-		if(height < 0 || height > 250) {
-			throw new IllegalArgumentException("Height cannot negative or larger than 250.");
-		}
-		
-		// Check whether sex is not null and not empty.
-		if(sex == null || sex.isEmpty()) {
-			throw new IllegalArgumentException("Sex can only be either 'male', 'female', or 'none'.");
-		}
-		
-		// Check whether the sex is equal to either 'male, 'female', or 'none'.
-		if(!"male".equals(sex.toLowerCase()) && !"female".equals(sex.toLowerCase()) && !"none".equals(sex.toLowerCase())) {
-			throw new IllegalArgumentException("Sex can only be either 'male', 'female', or 'none'.");
-		}
-		
-		// Check whether bloodType is not null.
+	public static boolean checkPatientInfo(Patient patient) {
+
+		validateFirstName(patient.getFirstName());
+		validateMiddleName(patient.getMiddleName());
+		validateLastName(patient.getLastName());
+		validateAge(patient.getAge());
+		validateWeight(patient.getWeight());
+		validateHeight(patient.getHeight());
+		validateSex(patient.getSex());
+		validateBloodType(patient.getBloodType());
+
+		return true;
+	}
+
+	private static boolean validateBloodType(com.swen6301.builder.types.BloodType bloodType) {
 		if(bloodType == null) {
 			throw new IllegalArgumentException("Blood type cannot be null.");
 		}
-		
-		// patient's information is correct, therefore return "true".
 		return true;
+	}
+
+	private static boolean validateSex(String sex) {
+		if(isNullOrEmpty(sex) || (!"male".equals(sex.toLowerCase()) && !"female".equals(sex.toLowerCase()) && !"none".equals(sex.toLowerCase()))) {
+			throw new IllegalArgumentException("Sex can only be either 'male', 'female', or 'none'.");
+		}
+		return true;
+	}
+
+	private static boolean validateHeight(int height) {
+		if(height < 0 || height > 250) {
+			throw new IllegalArgumentException("Height cannot negative or larger than 250.");
+		}
+		return true;
+	}
+
+	private static boolean validateWeight(int weight) {
+		if(weight < 0 || weight > 300) {
+			throw new IllegalArgumentException("Weight cannot negative or larger than 300.");
+		}
+		return true;
+	}
+
+	private static boolean validateAge(int age) {
+		if(age < 0 || age > 120) {
+			throw new IllegalArgumentException("Age cannot be negative or larger than 120.");
+		}
+		return true;
+	}
+
+	private static boolean validateLastName(String lastName) {
+		if(isNullOrEmpty(lastName)) {
+			throw new IllegalArgumentException("Last name cannot be empty of null.");
+		}
+		return true;
+	}
+
+	private static boolean validateMiddleName(String middleName) {
+		if(middleName == null) {
+			throw new IllegalArgumentException("Middle name cannot be null.");
+		}
+		return true;
+	}
+
+
+	private static boolean validateFirstName(String firstName){
+		if(isNullOrEmpty(firstName)) {
+			throw new IllegalArgumentException("First name cannot be empty of null.");
+		}
+		return true;
+	}
+
+	private static boolean isNullOrEmpty(String string){
+		return string == null || string.isEmpty();
 	}
 
 }
